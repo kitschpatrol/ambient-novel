baa2c81423672892f4ebe32276bd1f0fde8ac9f4
Eric Mika <eric@ericmika.com>
2023-06-11 17:06:03 -0400

    Add tweakpane.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
-	import type { BookData } from '$lib/schemas/bookSchema';
 	import Book from '$lib/components/Book.svelte';
+	import Tweakpane from '$lib/components/Tweakpane.svelte';
 	import bookDataRaw from '$lib/data/book.json';
+	import type { BookData } from '$lib/schemas/bookSchema';
 	const bookData = bookDataRaw as BookData;
 </script>
 
 <Book {bookData} />
+<Tweakpane />
