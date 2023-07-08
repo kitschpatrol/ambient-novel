075142dea896fd6c83eba80dce862542d754241c
Eric Mika <eric@ericmika.com>
2023-07-08 14:26:20 -0400

    Get Tweakpane from NPM.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
+	import { dev } from '$app/environment';
 	import Book from '$lib/components/Book.svelte';
-	// import Tweakpane from '$lib/components/Tweakpane.svelte';
 	import bookDataRaw from '$lib/data/book.json';
 	import type { BookData } from '$lib/schemas/bookSchema';
+	import Tweakpane from 'tweakpane-css/src/Tweakpane.svelte';
 	const bookData = bookDataRaw as BookData;
 </script>
 
 <Book {bookData} useQueryStore={true} />
-<!-- <Tweakpane /> -->
+
+{#if dev}
+	<Tweakpane />
+{/if}
