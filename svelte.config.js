decbaea6b57e84d5dcf4a57cefc964ffc7014d96
Eric Mika <eric@ericmika.com>
2023-10-03 22:38:17 -0400

    Give up on precompressed assets to avoid .gz download prompt problem.

--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,7 @@ const config = {
 	kit: {
 		adapter: adapter({
 			// cloudflare pages does this for us...
-			precompress: true,
+			precompress: false,
 			fallback: 'index.html'
 		}),
 		// serviceWorker: {
