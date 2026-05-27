89d14eef5723745c5d8ff76a7d95cea6e3e81f4e
Eric Mika <eric@ericmika.com>
2026-05-26 20:44:05 -0400

    Fix type error in book data loader.

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,8 +1,9 @@
 import bookDataRaw from '$lib/data/book.json'
+import { bookSchema } from '$lib/schemas/book-schema'
 import type { PageServerLoad } from './$types'
 
 // Needed when in layout.ts?
 // export const prerender = true;
 
 // eslint-disable-next-line ts/require-await
-export const load: PageServerLoad = async () => ({ bookData: bookDataRaw })
+export const load: PageServerLoad = async () => ({ bookData: bookSchema.parse(bookDataRaw) })
